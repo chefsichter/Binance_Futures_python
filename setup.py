b1e34110767d32b13c64d6369cc2ef6697d3e4a9
chefsichter <51502336+chefsichter@users.noreply.github.com>
2022-06-29 10:57:14 +0200

    cleared websocket-client==0.57.0 pinning

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 
 setup(
     name="binance-futures",
-    version="1.1.0",
-    packages=['binance_f', 'binance_f.impl', 'binance_f.impl.utils', 'binance_f.exception', 'binance_f.model', 'binance_f.base', 'binance_f.constant', 'binance_d', 'binance_d.impl', 'binance_d.impl.utils', 'binance_d.exception', 'binance_d.model', 'binance_d.base', 'binance_d.constant'],
-    install_requires=['requests', 'apscheduler', 'websocket-client==0.57.0', 'urllib3', 'tzlocal<3.0']
+    version="1.2.0",
+    packages=['binance_f', 'binance_f.impl', 'binance_f.impl.utils', 'binance_f.exception', 'binance_f.model',
+              'binance_f.base', 'binance_f.constant', 'binance_d', 'binance_d.impl', 'binance_d.impl.utils',
+              'binance_d.exception', 'binance_d.model', 'binance_d.base', 'binance_d.constant'],
+    install_requires=['requests', 'apscheduler', 'websocket-client', 'urllib3', 'tzlocal<3.0']
 )
 
